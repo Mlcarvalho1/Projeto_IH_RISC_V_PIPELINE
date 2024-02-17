e282de2c0f75e927377eb2db4a6b7e30da283e96
vsmn <69327389+Nether190@users.noreply.github.com>
2024-02-17 03:28:18 -0300

    Calculo correto de PC para JAL e JALR

--- design/BranchUnit.sv
+++ design/BranchUnit.sv
@@ -1,26 +1,45 @@
 `timescale 1ns / 1ps
 
 module BranchUnit #(parameter PC_W = 9) (
-    input  logic [PC_W-1:0] current_PC  , //! Current Program Counter
-    input  logic [    31:0] imm         , //! Generated immediate from imm_Gen
-    input  logic            branch      , //! Signal from the Controller that the current instruction is a branch
-    input  logic [    31:0] ALU_result  , //! Result from ALU comparison
-    output logic [    31:0] next_PC_imm , //! PC+immediate
-    output logic [    31:0] next_PC_four, //! PC+4
-    output logic [    31:0] branch_PC   , //! PC+immediate if branch is taken, otherwise 0
-    output logic            PC_sel        //! Signal to PC Mux wether branch will be taken
+    input  logic [PC_W-1:0] current_PC   , //! Current Program Counter
+    input  logic [    31:0] imm          , //! Generated immediate from imm_Gen
+    input  logic [     1:0] ctrl_transfer, //! Signal from the Controller that the current instruction is a branch
+    //00: No Control Transfer
+    //01: Branch
+    //10: JAL
+    //11: JALR
+    input  logic [    31:0] ALU_result   , //! Result from ALU comparison
+    output logic [    31:0] out          , //! No longer in use
+    output logic [    31:0] next_PC_four , //! PC+4
+    output logic [    31:0] branch_PC    , //! PC  depending on control transfer type
+    output logic            PC_sel         //! Signal to PC Mux wether branch will be taken
     //0: PC = PC+4
     //1: PC = branch_PC
 );
 
     logic [31:0] PC_full;
-    assign PC_full      = {23'b0, current_PC};
-    assign next_PC_imm  = PC_full + imm;
-    assign next_PC_four = PC_full + 32'b100;
 
-    logic branch_result;
-    assign branch_result = branch && ALU_result[0];
-    assign branch_PC     = (branch_result) ? next_PC_imm : 32'b0;
-    assign PC_sel        = branch_result;
+    always_comb begin
+        PC_full      = {23'b0, current_PC};
+        next_PC_four = PC_full + 32'b100;
+
+        case (ctrl_transfer)
+            2'b00 : begin // NO CTRL
+                PC_sel = 0;
+            end
+            2'b01 : begin // BRANCH
+                PC_sel    = ALU_result[0];
+                branch_PC = PC_full + imm;
+            end
+            2'b10 : begin // JAL
+                PC_sel    = 1;
+                branch_PC = PC_full + imm;
+            end
+            2'b11 : begin // JALR
+                PC_sel    = 1;
+                branch_PC = PC_full + ALU_result;
+            end
+        endcase
+    end
 
 endmodule
